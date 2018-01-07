37cbb70fa764686e8a4093820e001f2684d7e927
Nicholas Terrell <nicholas@terrell.com.au>
2018-01-07 13:17:44 +1100

    make ticker run every minute

--- ticker.go
+++ ticker.go
@@ -13,7 +13,7 @@ type TickerCallback func(ctx context.Context)
 // startTicker start the ticker and run the callback at each interval
 func startTicker(ctx context.Context, tick TickerCallback) {
 	go func() {
-		ticker := time.NewTicker(10 * time.Second)
+		ticker := time.NewTicker(1 * time.Minute)
 		// cleanup the ticker on return
 		defer ticker.Stop()
 		for {
